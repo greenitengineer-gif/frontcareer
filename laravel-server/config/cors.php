11ebf0aa7e3474ca5985c702f153b54284bb54b0
Ankhbayar Zoltuya <student@ADC.HUMANITIES.MN>
2026-04-28 12:13:01 +0800

    laravel

--- laravel-server/config/cors.php
+++ laravel-server/config/cors.php
@@ -12,7 +12,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://career.unetei.net', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
